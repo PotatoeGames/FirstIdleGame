776bb3da7fa9c66e47f6584f00d6fcb544123ca7
DPullarKhan <dpullarkhan@gmail.com>
2017-06-06 17:55:15 -0400

    Added Idle Test Features
    Put all stats and stat manager into Nick's Gym
    Stats automatically increment each second
    Stats are adjustable through buttons

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,38 +5,91 @@ using UnityEngine;
 //added includes
 using UnityEngine.UI;
 
-public class PlayerStats : MonoBehaviour {
+public class PlayerStats : MonoBehaviour 
+{
 	
-	[SerializeField] int playerEssence; 	  //basic useable currency
-	[SerializeField] int totalEssence;		  //total amount of currency acquired in current realm
+	[SerializeField] int playerEssence = 0; 	  //basic useable currency
+	[SerializeField] int totalEssence = 0;		  //total amount of currency acquired in current realm
 
-	[SerializeField] int baseValue; 		  //total value of current operation
+	[SerializeField] int baseValue = 0; 		  //total value of current operation
 
-	[SerializeField] int dronePopulation; 	  //number of active drones
-	[SerializeField] float droneReproduction; //rate of increase in amount of drones
-	[SerializeField] float droneValue; 		  //harvesting multiplier
+	[SerializeField] int dronePopulation = 0; 	  //number of active drones
+	[SerializeField] int droneReproduction = 0;   //rate of increase in amount of drones
+	[SerializeField] float droneValue = 1; 		  //harvesting multiplier
 
-	[SerializeField] int essenceProduction;   //essence harvesting rate
-	[SerializeField] int idleProduction; 	  //essence harvesting rate when idle
-	[SerializeField] float idleMultiplier;    //scalar for essence production when app is closed
+	[SerializeField] int essenceProduction = 0;   //essence harvesting rate
+	[SerializeField] int idleProduction = 0; 	  //essence harvesting rate when idle
+	[SerializeField] float idleMultiplier = 10;    //scalar for essence production when app is closed
 
-	[SerializeField] int playerCredits; 	  //amount of permanent-upgrade currency
+	[SerializeField] int playerCredits = 0; 	  //amount of permanent-upgrade currency
 
-	[SerializeField] int playerDarkMatter;    //amount of direct-multiplying currency
-	[SerializeField] int darkMatterBonus;     //percentage bonus from total dark matter
+	[SerializeField] int playerDarkMatter = 0;    //amount of direct-multiplying currency
+	[SerializeField] int darkMatterBonus = 100;     //percentage bonus from total dark matter
 
 	public Text statText;
 
-	void Start () {
+	void Start () 
+	{
 		statText = GameObject.Find ("Canvas/StatText").GetComponent<Text> ();
+		StartCoroutine (IncrementStats());
 	}
 
-	void Update () {
+	void Update () 
+	{
 		TestStatUpdate ();
 	}
 
 	void TestStatUpdate() 
 	{
-		statText.text = "Player Essence = " + playerEssence;
+		statText.text = 
+			"Player Essence = " + playerEssence + '\n' + 
+			"Total Essence = " + totalEssence + '\n' +
+			"Drone Population = " + dronePopulation + '\n' + 
+			"Drone Reproduction = " + droneReproduction + '\n' + 
+			"Drone Value = " + droneValue + '\n' + 
+			"Essence Production = " + essenceProduction + '\n' + 
+			"Idle Production = " + idleProduction + '\n' + 
+			"Idle Multiplier = " + idleMultiplier + "% \n" +
+			"Player Credits = " + playerCredits + '\n' + 
+			"Player Dark Matter = " + playerDarkMatter + '\n' +
+			"Dark Matter Bonus = " + darkMatterBonus + '%';
+	}
+
+	IEnumerator IncrementStats()
+	{
+		yield return new WaitForSeconds (1);
+		dronePopulation += droneReproduction;
+		essenceProduction = Mathf.RoundToInt(droneValue * dronePopulation * (darkMatterBonus / 100));
+		idleProduction = Mathf.RoundToInt(essenceProduction * idleMultiplier / 100);
+		playerEssence += essenceProduction;
+		totalEssence += essenceProduction;
+		StartCoroutine (IncrementStats());
+	}
+
+	public void AddDrone()
+	{
+		dronePopulation++;
+	}
+
+	public void AddDroneMultiplier()
+	{
+		droneValue += 0.1f;
+	}
+
+	public void AddDarkMatter()
+	{
+		playerDarkMatter += 1;
+		darkMatterBonus = 100 + (playerDarkMatter * 10);
+	}
+
+	public void AddIdleMultiplier()
+	{
+		if (idleMultiplier < 100)
+		idleMultiplier += 10;
+	}
+
+	public void AddDroneReproduction()
+	{
+		droneReproduction += 1;
 	}
 }
--- Assets/Scripts/Test Scripts/Jump.cs
+++ Assets/Scripts/Test Scripts/Jump.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour {
+public class Jump : MonoBehaviour 
+{
 	public int bounceFactor;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		bounceFactor = 100;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 
-	public void Bounce() {
+	public void Bounce() 
+	{
 		this.GetComponent<Rigidbody> ().AddForce(new Vector3(0,bounceFactor,0));
 		bounceFactor += 10;
 	}
