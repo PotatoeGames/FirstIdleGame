42a7c6a9bb4fc1a08a113c0c442912d3fe62e80e
DPullarKhan <dpullarkhan@gmail.com>
2017-06-06 17:21:15 -0400

    Created Main Game and Menu Scenes
    Moved test scenes/scripts to own folders
    Set up player stats script and added to stat manager object in game scene

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//added includes
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+	
+	[SerializeField] int playerEssence; 	  //basic useable currency
+	[SerializeField] int totalEssence;		  //total amount of currency acquired in current realm
+
+	[SerializeField] int baseValue; 		  //total value of current operation
+
+	[SerializeField] int dronePopulation; 	  //number of active drones
+	[SerializeField] float droneReproduction; //rate of increase in amount of drones
+	[SerializeField] float droneValue; 		  //harvesting multiplier
+
+	[SerializeField] int essenceProduction;   //essence harvesting rate
+	[SerializeField] int idleProduction; 	  //essence harvesting rate when idle
+	[SerializeField] float idleMultiplier;    //scalar for essence production when app is closed
+
+	[SerializeField] int playerCredits; 	  //amount of permanent-upgrade currency
+
+	[SerializeField] int playerDarkMatter;    //amount of direct-multiplying currency
+	[SerializeField] int darkMatterBonus;     //percentage bonus from total dark matter
+
+	public Text statText;
+
+	void Start () {
+		statText = GameObject.Find ("Canvas/StatText").GetComponent<Text> ();
+	}
+
+	void Update () {
+		TestStatUpdate ();
+	}
+
+	void TestStatUpdate() 
+	{
+		statText.text = "Player Essence = " + playerEssence;
+	}
+}
