f57c5d34b9e676887de3807f935ad3cd3b061d93
raimuriel <jarrettjdeans@gmail.com>
2017-06-06 15:46:56 -0400

    Basic Clicking

---
+++ Assets/Scripts/TestClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//my includes
+using UnityEngine.UI;
+
+public class TestClick : MonoBehaviour
+{
+    public Text clickCounter;
+    public float clicks;
+    public float countPerClick;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        clickCounter.text = "Counter: " + clicks;
+
+	}
+
+    public void clicked ()
+    {
+        clicks += countPerClick;
+
+    }
+}
