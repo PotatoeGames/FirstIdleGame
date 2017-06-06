5dc94de3f27d06d80d257bf4ac32c85579aa588a
DPullarKhan <dpullarkhan@gmail.com>
2017-06-06 16:34:52 -0400

    Created Nick's Gym Messed around with button click functions moving objects in game world

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+	public int bounceFactor;
+	// Use this for initialization
+	void Start () {
+		bounceFactor = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Bounce() {
+		this.GetComponent<Rigidbody> ().AddForce(new Vector3(0,bounceFactor,0));
+		bounceFactor += 10;
+	}
+}
